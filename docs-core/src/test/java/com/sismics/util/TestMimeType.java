c00b6d3b8740ef7864b997e15711a0d860c54494
fest6 <174621678+fest6@users.noreply.github.com>
2025-04-09 11:15:48 +0800

    Create TestMimeType.java

---
+++ docs-core/src/test/java/com/sismics/util/TestMimeType.java
@@ -0,0 +1,78 @@
+package com.sismics.util;
+
+import com.sismics.util.mime.MimeType;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MimeTypeTest extends BaseTest {
+
+    @Test
+    public void testImagePng() {
+        Assert.assertEquals("image/png", MimeType.IMAGE_PNG);
+    }
+    
+    @Test
+    public void testImageJpeg() {
+        Assert.assertEquals("image/jpeg", MimeType.IMAGE_JPEG);
+    }
+    
+    @Test
+    public void testImageGif() {
+        Assert.assertEquals("image/gif", MimeType.IMAGE_GIF);
+    }
+    
+    @Test
+    public void testApplicationZip() {
+        Assert.assertEquals("application/zip", MimeType.APPLICATION_ZIP);
+    }
+    
+    @Test
+    public void testApplicationPdf() {
+        Assert.assertEquals("application/pdf", MimeType.APPLICATION_PDF);
+    }
+    
+    @Test
+    public void testOpenDocumentText() {
+        Assert.assertEquals("application/vnd.oasis.opendocument.text", MimeType.OPEN_DOCUMENT_TEXT);
+    }
+    
+    @Test
+    public void testOfficeDocument() {
+        Assert.assertEquals("application/vnd.openxmlformats-officedocument.wordprocessingml.document", MimeType.OFFICE_DOCUMENT);
+    }
+    
+    @Test
+    public void testOfficePresentation() {
+        Assert.assertEquals("application/vnd.openxmlformats-officedocument.presentationml.presentation", MimeType.OFFICE_PRESENTATION);
+    }
+    
+    @Test
+    public void testOfficeSheet() {
+        Assert.assertEquals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", MimeType.OFFICE_SHEET);
+    }
+    
+    @Test
+    public void testTextPlain() {
+        Assert.assertEquals("text/plain", MimeType.TEXT_PLAIN);
+    }
+    
+    @Test
+    public void testTextCsv() {
+        Assert.assertEquals("text/csv", MimeType.TEXT_CSV);
+    }
+    
+    @Test
+    public void testVideoWebm() {
+        Assert.assertEquals("video/webm", MimeType.VIDEO_WEBM);
+    }
+    
+    @Test
+    public void testVideoMp4() {
+        Assert.assertEquals("video/mp4", MimeType.VIDEO_MP4);
+    }
+    
+    @Test
+    public void testDefault() {
+        Assert.assertEquals("application/octet-stream", MimeType.DEFAULT);
+    }
+}
